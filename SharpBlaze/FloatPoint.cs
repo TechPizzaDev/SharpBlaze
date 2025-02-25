b73dc4e8aa61a3edc4b5da47d8ca718907b6abef
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-02-25 20:57:06 +0100

    Fix FloatPoint equality

--- SharpBlaze/FloatPoint.cs
+++ SharpBlaze/FloatPoint.cs
@@ -1,12 +1,14 @@
+using System;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics;
 
 namespace SharpBlaze;
 
-[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
-public struct FloatPoint
+[DebuggerDisplay($"{{{nameof(ToString)}(),nq}}")]
+public struct FloatPoint : IEquatable<FloatPoint>
 {
     public double X;
     public double Y;
@@ -19,49 +21,82 @@ public struct FloatPoint
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public FloatPoint(double value)
+    {
+        X = value;
+        Y = value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public FloatPoint(Vector128<double> xy)
     {
         this = Unsafe.BitCast<Vector128<double>, FloatPoint>(xy);
     }
-
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly Vector128<double> AsVector128()
     {
         return Unsafe.BitCast<FloatPoint, Vector128<double>>(this);
     }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public readonly FloatPoint Clamp(FloatPoint min, FloatPoint max)
+    {
+        Vector128<double> left = Vector128.Max(AsVector128(), min.AsVector128());
+        return new FloatPoint(Vector128.Min(left, max.AsVector128()));
+    }
 
+    public bool Equals(FloatPoint other)
+    {
+        return this == other;
+    }
+
+    public override bool Equals([NotNullWhen(true)] object? obj)
+    {
+        return obj is FloatPoint other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y);
+    }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static FloatPoint operator -(FloatPoint a, FloatPoint b)
     {
-        return new FloatPoint(
-            a.X - b.X,
-            a.Y - b.Y
-        );
+        return new FloatPoint(a.AsVector128() - b.AsVector128());
     }
-
-
+    
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static FloatPoint operator +(FloatPoint a, FloatPoint b)
     {
-        return new FloatPoint(
-            a.X + b.X,
-            a.Y + b.Y
-        );
+        return new FloatPoint(a.AsVector128() + b.AsVector128());
     }
-
-
+    
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static FloatPoint operator *(FloatPoint a, FloatPoint b)
     {
-        return new FloatPoint(
-            a.X * b.X,
-            a.Y * b.Y
-        );
+        return new FloatPoint(a.AsVector128() * b.AsVector128());
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static FloatPoint operator /(FloatPoint a, FloatPoint b)
+    {
+        return new FloatPoint(a.AsVector128() / b.AsVector128());
     }
 
-    private readonly string GetDebuggerDisplay()
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator ==(FloatPoint a, FloatPoint b)
+    {
+        return Vector128.EqualsAll(a.AsVector128(), b.AsVector128());
+    }
+
+    public static bool operator !=(FloatPoint a, FloatPoint b)
+    {
+        return !(a == b);
+    }
+
+    public override string ToString()
     {
         string separator = NumberFormatInfo.GetInstance(null).NumberGroupSeparator;
 
