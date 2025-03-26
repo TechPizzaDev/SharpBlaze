a01aaeb2b28a827cd83670f0b1b4cd42a106e640
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-03-26 21:41:07 +0100

    Try using Vector64 in IntRect

--- SharpBlaze.Numerics/IntRect.cs
+++ SharpBlaze.Numerics/IntRect.cs
@@ -1,8 +1,7 @@
-using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics;
 
 namespace SharpBlaze;
-
 
 public readonly struct IntRect
 {
@@ -26,6 +25,7 @@ public readonly struct IntRect
 
     public int Height => _value.GetElement(3) - _value.GetElement(1);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static IntRect FromMinMax(int x1, int y1, int x2, int y2)
     {
         return new(Vector128.Create(x1, y1, x2, y2));
@@ -36,19 +36,47 @@ public readonly struct IntRect
         return new IntRect(minMax);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static IntRect Union(IntRect a, IntRect b)
     {
-        Vector128<int> min = Vector128.Min(a._value, b._value);
-        Vector128<int> max = Vector128.Max(a._value, b._value);
-        Vector128<int> mix = Vector128.ConditionalSelect(Vector128.Create(~0, ~0, 0, 0), min, max);
+        Vector128<int> vA = a._value;
+        Vector128<int> vB = b._value;
+        
+        Vector128<int> mix;
+        if (Vector64.IsHardwareAccelerated)
+        {
+            Vector64<int> min = Vector64.Min(vA.GetLower(), vB.GetLower());
+            Vector64<int> max = Vector64.Max(vA.GetUpper(), vB.GetUpper());
+            mix = Vector128.Create(min, max);
+        }
+        else
+        {
+            Vector128<int> min = Vector128.Min(vA, vB);
+            Vector128<int> max = Vector128.Max(vA, vB);
+            mix = Vector128.ConditionalSelect(Vector128.Create(~0, ~0, 0, 0), min, max);
+        }
         return new IntRect(mix);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static IntRect Intersect(IntRect a, IntRect b)
     {
-        Vector128<int> min = Vector128.Min(a._value, b._value);
-        Vector128<int> max = Vector128.Max(a._value, b._value);
-        Vector128<int> mix = Vector128.ConditionalSelect(Vector128.Create(~0, ~0, 0, 0), max, min);
+        Vector128<int> vA = a._value;
+        Vector128<int> vB = b._value;
+        
+        Vector128<int> mix;
+        if (Vector64.IsHardwareAccelerated)
+        {
+            Vector64<int> min = Vector64.Min(vA.GetLower(), vB.GetLower());
+            Vector64<int> max = Vector64.Max(vA.GetUpper(), vB.GetUpper());
+            mix = Vector128.Create(max, min);
+        }
+        else
+        {
+            Vector128<int> min = Vector128.Min(vA, vB);
+            Vector128<int> max = Vector128.Max(vA, vB);
+            mix = Vector128.ConditionalSelect(Vector128.Create(~0, ~0, 0, 0), max, min);
+        }
         return new IntRect(mix);
     }
 
@@ -57,14 +85,29 @@ public readonly struct IntRect
         return _value;
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public bool Contains(IntRect other)
     {
-        Vector128<int> min = Vector128.GreaterThanOrEqual(_value, other._value);
-        Vector128<int> max = Vector128.LessThanOrEqual(_value, other._value);
-        Vector128<int> mix = Vector128.ConditionalSelect(Vector128.Create(~0, ~0, 0, 0), min, max);
+        Vector128<int> vA = _value;
+        Vector128<int> vB = other._value;
+
+        Vector128<int> mix;
+        if (Vector64.IsHardwareAccelerated)
+        {
+            Vector64<int> min = Vector64.GreaterThanOrEqual(vB.GetLower(), vA.GetLower());
+            Vector64<int> max = Vector64.LessThanOrEqual(vB.GetUpper(), vA.GetUpper());
+            mix = Vector128.Create(min, max);
+        }
+        else
+        {
+            Vector128<int> min = Vector128.GreaterThanOrEqual(vB, vA);
+            Vector128<int> max = Vector128.LessThanOrEqual(vB, vA);
+            mix = Vector128.ConditionalSelect(Vector128.Create(~0, ~0, 0, 0), min, max);
+        }
         return Vector128.EqualsAll(mix, Vector128<int>.AllBitsSet);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void Deconstruct(out int X1, out int Y1, out int X2, out int Y2)
     {
         X1 = _value.GetElement(0);
@@ -73,11 +116,20 @@ public readonly struct IntRect
         Y2 = _value.GetElement(3);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public bool HasArea()
     {
-        (Vector128<long> left, Vector128<long> right) = Vector128.Widen(_value);
+        Vector128<int> v = _value;
+        
+        if (Vector64.IsHardwareAccelerated)
+        {
+            return Vector64.LessThanAll(v.GetLower(), v.GetUpper());
+        }
+
+        (Vector128<long> left, Vector128<long> right) = Vector128.Widen(v);
         return Vector128.LessThanAll(left, right);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static IntRect operator +(IntRect a, IntRect b) => new(a._value + b._value);
 }
