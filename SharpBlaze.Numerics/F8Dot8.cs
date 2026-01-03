fe3aa353cc5d22ecaf29a763b79cba7f48d4543c
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2026-01-04 00:29:19 +0100

    Use primary constructor

--- SharpBlaze.Numerics/F8Dot8.cs
+++ SharpBlaze.Numerics/F8Dot8.cs
@@ -32,18 +32,10 @@ public readonly partial struct F8Dot8
 }
 
 [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
-public readonly struct F8Dot8x2
+public readonly struct F8Dot8x2(uint value)
 {
-    private readonly uint _value;
-
-    private F8Dot8x2(uint value) => _value = value;
-
-    public F8Dot8 X => new((short) _value);
-    public F8Dot8 Y => new((short) (_value >> 16));
-
-    public static explicit operator uint(F8Dot8x2 value) => value._value;
-
-    public static explicit operator F8Dot8x2(uint value) => new(value);
+    public F8Dot8 X => new((short) value);
+    public F8Dot8 Y => new((short) (value >> 16));
 
     private string GetDebuggerDisplay()
     {
@@ -54,20 +46,12 @@ public readonly struct F8Dot8x2
 }
 
 [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
-public readonly struct F8Dot8x4
+public readonly struct F8Dot8x4(ulong value)
 {
-    private readonly ulong _value;
-
-    private F8Dot8x4(ulong value) => _value = value;
-
-    public F8Dot8 X => new((short) _value);
-    public F8Dot8 Y => new((short) (_value >> 16));
-    public F8Dot8 Z => new((short) (_value >> 32));
-    public F8Dot8 W => new((short) (_value >> 48));
-
-    public static explicit operator ulong(F8Dot8x4 value) => value._value;
-
-    public static explicit operator F8Dot8x4(ulong value) => new(value);
+    public F8Dot8 X => new((short) value);
+    public F8Dot8 Y => new((short) (value >> 16));
+    public F8Dot8 Z => new((short) (value >> 32));
+    public F8Dot8 W => new((short) (value >> 48));
 
     private string GetDebuggerDisplay()
     {
@@ -86,7 +70,7 @@ public readonly partial struct F8Dot8
         Debug.Assert((a.ToBits() & 0xffff0000) == 0);
         Debug.Assert((b.ToBits() & 0xffff0000) == 0);
 
-        return (F8Dot8x2) (uint) (a._value | (b._value << 16));
+        return new F8Dot8x2((uint) (a._value | (b._value << 16)));
     }
 
 
@@ -101,7 +85,7 @@ public readonly partial struct F8Dot8
 
         uint lo = (uint) a._value | ((uint) b._value << 16);
         ulong hi = ((ulong) c._value << 32) | ((ulong) d._value << 48);
-        
-        return (F8Dot8x4) (lo | hi);
+
+        return new F8Dot8x4(lo | hi);
     }
 }
